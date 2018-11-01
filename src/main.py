704af7293960e02bb22d6b567fa9ba9bfbb27114
vergil <vergil@localhost.localdomain>
2018-11-01 09:11:27 -0400

    init

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+import sys
+from models.gemini import gemini
+def main():
+    gemini.train_model()
+
+if __name__ == '__main__':
+    main()
---
+++ src/models/gemini/gemini.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+import tensorflow as tf
+
+def train_model(dataset_path, model_path, log_path):
+    print('Trainging...')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
